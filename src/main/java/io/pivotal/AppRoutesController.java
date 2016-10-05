fb49bb5bd94ca09ee388d94209b005fe5d1dd556
Bjorn Boe <bboe@pivotal.io>
2016-10-05 16:57:44 -0400

    Another bug fix

--- src/main/java/io/pivotal/AppRoutesController.java
+++ src/main/java/io/pivotal/AppRoutesController.java
@@ -29,7 +29,7 @@ public class AppRoutesController {
 
     @RequestMapping("/")
     public Map getRoutes() {
-        System.out.printf("AppRoutesController.getRoutes Route path: %s, Username: %s, Password: %s", routePath, routeUsername, routePassword);
+        System.out.printf("AppRoutesController.getRoutes Route path: %s, Username: %s, Password: %s\n", routePath, routeUsername, routePassword);
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> request = new HttpEntity<String>(createHeaders(routeUsername, routePassword));
 
